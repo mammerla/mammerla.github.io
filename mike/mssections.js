37f87dd5960459ecf785aa217716e2f6e84ae5aa
Mike Ammerlaan <ammerlaan@live.com>
2016-10-25 12:56:00 -0700

    Updated links to Tech Summit code samples

--- mike/mssections.js
+++ mike/mssections.js
@@ -1,18 +1,18 @@
 ﻿[{
     "items": [
-  /*      {
+        {
             "id": "tsgsamples",
             "title": "Tech Summit - Microsoft Graph Samples",
             "url": "https://graph.microsoft.com",
-            "content": "",
+            "content": "Thanks everyone for dropping into my Microsoft Graph Tech Summit Session! The following samples were used in Tech Summit sessions around the Microsoft Graph:<br/>&#160;<br/><ul><li>Basic Microsoft Graph site:<br/><a href='https://github.com/mammerla/microsoftgraph-js-basicsite'>https://github.com/mammerla/microsoftgraph-js-basicsite</a></li><li>Excel Bot:<br/><a href='https://github.com/microsoftgraph/botframework-csharp-excelbot-rest-sample'>https://github.com/microsoftgraph/botframework-csharp-excelbot-rest-sample</a></li><li>Basic Xamarin and Azure Active Directory:<br/><a href='https://github.com/Azure-Samples/active-directory-xamarin-native-v2'>https://github.com/Azure-Samples/active-directory-xamarin-native-v2</a></li><li>Xamarin Property Manager:<br/><a href='https://github.com/microsoftgraph/xamarin-csharp-propertymanager-sample'>https://github.com/microsoftgraph/xamarin-csharp-propertymanager-sample</a></li><li>Open ID Connect:<br/><a href='https://github.com/Azure-Samples/active-directory-dotnet-webapp-openidconnect'>https://github.com/Azure-Samples/active-directory-dotnet-webapp-openidconnect</a></li></ul>",
             "mainImage": "https://www.microsoft.com/en-us/CMSImages/Card_Prague_VP4.jpg?version=8fecec87-67b4-c66a-83bf-d6d0592136b6&CollectionId=09133b91-16d8-445f-bffd-3d49dfb2c490"
         },
         {
             "id": "tsgspfxamples",
             "title": "Tech Summit - SharePoint Framework Samples",
             "url": "https://dev.office.com/sharepoint",
-            "content": "",
+            "content": "Hope you found the SharePoint Framework Tech Summit Session useful.  Here are the two samples I used in the Tech Summit Session:<br/>&#160;<br/><ul><li>Basic Create-from-Scratch Web Part:<br/><a href='https://github.com/mammerla/spfx-basic-webpart'>https://github.com/mammerla/spfx-basic-webpart</a></li><li>jQuery UI, SharePoint List, and Property Panes:<br/><a href='https://github.com/wbaer/SPFx-JQueryUI-Widget-Webpart'>https://github.com/wbaer/SPFx-JQueryUI-Widget-Webpart</a></li></ul>",
             "mainImage": "https://www.microsoft.com/en-us/CMSImages/Card_Prague_VP4.jpg?version=8fecec87-67b4-c66a-83bf-d6d0592136b6&CollectionId=09133b91-16d8-445f-bffd-3d49dfb2c490"
-        }*/
+        }
         ]  
 }]
