44992c874ba279fb9bf769e63015b890311f5f68
Mike Ammerlaan <mike@bendyline.com>
2016-10-25 05:52:12 -0700

    Updates to personal site

---
+++ mike/mssections.js
@@ -0,0 +1,18 @@
+﻿[{
+    "items": [
+  /*      {
+            "id": "tsgsamples",
+            "title": "Tech Summit - Microsoft Graph Samples",
+            "url": "https://graph.microsoft.com",
+            "content": "",
+            "mainImage": "https://www.microsoft.com/en-us/CMSImages/Card_Prague_VP4.jpg?version=8fecec87-67b4-c66a-83bf-d6d0592136b6&CollectionId=09133b91-16d8-445f-bffd-3d49dfb2c490"
+        },
+        {
+            "id": "tsgspfxamples",
+            "title": "Tech Summit - SharePoint Framework Samples",
+            "url": "https://dev.office.com/sharepoint",
+            "content": "",
+            "mainImage": "https://www.microsoft.com/en-us/CMSImages/Card_Prague_VP4.jpg?version=8fecec87-67b4-c66a-83bf-d6d0592136b6&CollectionId=09133b91-16d8-445f-bffd-3d49dfb2c490"
+        }*/
+        ]  
+}]
